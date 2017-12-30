151e03c1b7274118b1937cd3eb67a3634e69a2e2
inxoy <i15098@student.htlwrn.ac.at>
2017-12-30 22:27:00 +0100

    Add PrototypePattern

---
+++ CreationalPatterns/05_PrototypePattern/src/Shape.java
@@ -0,0 +1,39 @@
+public abstract class Shape implements Cloneable {
+
+
+    abstract void draw();
+
+
+    public String getType(){
+        return type;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public Object clone() {
+        Object clone = null;
+
+        try {
+            clone = super.clone();
+
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        return clone;
+    }
+
+
+    private String id;
+    protected String type;
+
+}
